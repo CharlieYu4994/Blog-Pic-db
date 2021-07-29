c914538e327a311f19c7a48a78f2b31623863e4e
CharlieYu4994 <charlieyu4994@outlook.com>
2021-07-29 16:54:43 +0800

    加入 APOD 支持

--- genCSV.py
+++ genCSV.py
@@ -1,13 +1,16 @@
 import sqlite3
 
-def read_all(_db):
+def read_all(_table, _db):
     _cursor = _db.cursor()
-    _ret = _cursor.execute(f"SELECT * FROM bing").fetchall()
+    _ret = _cursor.execute(f"SELECT * FROM {_table}").fetchall()
     return _ret
 
 if __name__ == "__main__":
     database = sqlite3.connect("./picture.db")
-    tmp = read_all(database)
-    with open("./picture.csv", "w") as f:
-        for pic in tmp:
+    with open("./bing.csv", "w") as f:
+        for pic in read_all("bing", database):
+            f.write(f"{pic[0]},{pic[1]},{pic[2]}\n")
+    
+    with open("./apod.csv", "w") as f:
+        for pic in read_all("apod", database):
             f.write(f"{pic[0]},{pic[1]},{pic[2]}\n")
--- update.py
+++ update.py
@@ -1,22 +1,51 @@
-import sqlite3, requests, json
+import sqlite3, requests, json, time, re
 
-def add_line(_date, _url, _db):
+def add_line(_table, _date, _url, _db):
     _cursor = _db.cursor()
-    _cursor.execute(f"""INSERT INTO bing (DATE, BURL) VALUES ('{_date}', '{_url}')""")
+    _cursor.execute(f"""INSERT INTO {_table} (DATE, BURL) VALUES ('{_date}', '{_url}')""")
     _db.commit()
 
-def chk_line(_date, _db):
+def chk_line(_table, _date, _db):
     _cursor = _db.cursor()
-    _cursor.execute(f"""SELECT IFNULL((SELECT DATE FROM bing WHERE DATE='{_date}'), "NULL")""")
-    tmp = _cursor.fetchall()[0][0]
+    _cursor.execute(f"""SELECT IFNULL((SELECT DATE FROM {_table} WHERE DATE='{_date}'), "NULL")""")
+    tmp = _cursor.fetchone()[0]
     return False if tmp == "NULL" else True
 
-if __name__ == "__main__":
-    database = sqlite3.connect('./picture.db')
+def update_bing(_db):
     res = requests.get("https://cn.bing.com/HPImageArchive.aspx?format=js&idx=-1&n=9&mkt=zh-cn")
     tmp = json.loads(res.content.decode())["images"]
     for pic in tmp[::-1]:
-        if not chk_line(pic["enddate"], database):
-            add_line(pic["enddate"], pic["urlbase"], database)
-        
+        if not chk_line("bing", pic["enddate"], _db):
+            add_line("bing", pic["enddate"], pic["urlbase"], _db)
+
+def update_apod(_db):
+    macher = re.compile(r"image/.*\..{3}")
+    date = time.time()
+    pics = list()
+
+    for _ in range(5):
+        date -= 3600*24
+        tmp = time.strftime("%Y%m%d", time.localtime(date))
+
+        resp = requests.get(f"https://apod.nasa.gov/apod/ap{tmp[2:]}.html")
+        if resp.status_code != 200:
+            continue
+
+        url = macher.findall(resp.content.decode())
+        if url == []:
+            continue
+
+        pics.append({
+            "url": url[1],
+            "date": tmp
+        })
     
+    for pic in pics[::-1]:
+        print(pic)
+        if not chk_line("apod", pic["date"], _db):
+            add_line("apod", pic["date"], pic["url"], _db)
+
+if __name__ == "__main__":
+    database = sqlite3.connect('./picture.db')
+    update_bing(database)
+    update_apod(database)
